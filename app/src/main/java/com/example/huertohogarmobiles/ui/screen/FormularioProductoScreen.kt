8cb33a55b701d00804185c863a47f29b267c98a9
cyyyyyyyyri <soleceagpm@gmail.com>
2025-12-08 22:36:13 -0300

    ojala este commit se aproxime al commit final

--- app/src/main/java/com/example/huertohogarmobiles/ui/screen/FormularioProductoScreen.kt
+++ app/src/main/java/com/example/huertohogarmobiles/ui/screen/FormularioProductoScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -56,7 +57,7 @@ fun FormularioProductoScreen(
                 title = { Text(tituloPantalla) },
                 navigationIcon = {
                     IconButton(onClick = onBackClick) {
-                        Icon(Icons.Default.ArrowBack, "Volver")
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, "Volver")
                     }
                 }
             )
